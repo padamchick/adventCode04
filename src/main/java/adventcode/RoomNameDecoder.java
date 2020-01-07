8b24c0262ad35bd14ec707d18d4042c98b2319e9
Piotrek <piotrek.adamczykk@gmail.com>
2020-01-08 00:06:47 +0100

    Improve some methods using Stream API

--- src/main/java/adventcode/RoomNameDecoder.java
+++ src/main/java/adventcode/RoomNameDecoder.java
@@ -2,12 +2,7 @@ package adventcode;
 
 import adventcode.exception.InvalidRoomNameException;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -32,46 +27,58 @@ public class RoomNameDecoder {
     }
 
     public boolean isCorrectChecksum() {
-        if(!correctChecksum.equals(decodedChecksum)) {
+        if (!correctChecksum.equals(decodedChecksum)) {
             return false;
         }
         return true;
     }
 
     private String computeCorrectChecksum() {
-        Map<Character,Integer> letters = countLetters();
-        List<Map.Entry<Character,Integer>> sortedLetters = sortByLettersCount(letters);
+        Map<String, Long> letters = countLetters();
+        List<Map.Entry<String, Long>> sortedLetters = sortByLettersCount(letters);
         int checkSumLength = Math.min(5, sortedLetters.size());
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<checkSumLength; i++) {
+        for (int i = 0; i < checkSumLength; i++) {
             sb.append(sortedLetters.get(i).getKey());
         }
         return sb.toString();
     }
 
-    private Map<Character, Integer> countLetters() {
-        Map<Character, Integer> letters = new HashMap<>();
-        String allLetters = roomName.replaceAll("-", "");
-        for (Character letter : allLetters.toCharArray()) {
-            int letterCount = 1;
-            if (letters.containsKey(letter)) {
-                letterCount += letters.get(letter);
-            }
-            letters.put(letter, letterCount);
-        }
-        return letters;
+    private Map<String, Long> countLetters() {
+        Map<String, Long> frequentChars = Arrays.stream(roomName.replaceAll("-", "").split(""))
+                .collect(Collectors.groupingBy(c->c,Collectors.counting()));
+
+//  II SPOSOB
+//        Map<Character, Integer> letters = new HashMap<>();
+//        String allLetters = roomName.replaceAll("-", "");
+//        for (char c : allLetters.toCharArray()) {
+//            letters.merge(c,                  // key = char
+//                    1,                  // value to merge
+//                    Integer::sum);      // counting
+//        }
+
+//  III SPOSOB
+//        for (Character letter : allLetters.toCharArray()) {
+//            int letterCount = 1;
+//            if (letters.containsKey(letter)) {
+//                letterCount += letters.get(letter);
+//            }
+//            letters.put(letter, letterCount);
+//        }
+        return frequentChars;
     }
 
-    private List<Map.Entry<Character, Integer>> sortByLettersCount(Map<Character, Integer> letters) {
-        List<Map.Entry<Character, Integer>> entries = new ArrayList<>(letters.entrySet());
-//        entries = entries.stream().sorted(Map.Entry.<Character, Integer>comparingByValue(Comparator.reverseOrder())
-//                .thenComparing(Map.Entry.comparingByKey()))
-//                .collect(Collectors.toList());
-        Collections.sort(entries, (o1, o2) -> {
-            if (o2.getValue().compareTo(o1.getValue()) != 0)
-                return o2.getValue().compareTo(o1.getValue());
-            else return o1.getKey().compareTo(o2.getKey());
-        });
+    private List<Map.Entry<String, Long>> sortByLettersCount(Map<String, Long> letters) {
+        List<Map.Entry<String, Long>> entries = new ArrayList<>(letters.entrySet());
+        entries = entries.stream().sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder())
+                .thenComparing(Map.Entry.comparingByKey()))
+                .collect(Collectors.toList());
+//  SPOSOB II
+//        Collections.sort(entries, (o1, o2) -> {
+//            if (o2.getValue().compareTo(o1.getValue()) != 0)
+//                return o2.getValue().compareTo(o1.getValue());
+//            else return o1.getKey().compareTo(o2.getKey());
+//        });
 
         return entries;
     }
